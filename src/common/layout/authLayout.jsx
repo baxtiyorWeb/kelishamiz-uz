bdba3ed9e7458380763ff3fe1183bf1aa6574337
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-02-03 04:49:48 +0500

    yangilandi

--- src/common/layout/authLayout.jsx
+++ src/common/layout/authLayout.jsx
@@ -1,16 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 const AuthLayout = () => {
-  return (
-    <div className="flex h-[100vh] w-full flex-col items-center justify-center">
-      <h1 className="mb-10 mt-1 text-3xl">
-        <span className="text-teal-500">Kelishamiz.uz</span> saytiga hush
-        kelibsiz
-      </h1>
-      <div className="flex h-[780px] w-[650px] items-center justify-center bg-white shadow-lg">
-        <Outlet />
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex h-[100vh] w-full flex-col items-center justify-center'>
+			<h1 className='mb-10 mt-1 text-3xl'>
+				<span className='text-teal-500'>Kelishamiz.uz</span> saytiga hush
+				kelibsiz
+				<h1>yagilandi</h1>
+			</h1>
+			<div className='flex h-[780px] w-[650px] items-center justify-center bg-white shadow-lg'>
+				<Outlet />
+			</div>
+		</div>
+	);
 };
 export default AuthLayout;
