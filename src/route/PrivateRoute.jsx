6ea1518eceb934cf566ecee5210ec666fe681188
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2025-04-19 08:11:44 +0500

    fix: change PrivateRoute to default export

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -2,9 +2,11 @@
 import { Navigate } from 'react-router-dom';
 import useAuthStore from '../store';
 
-export const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children }) => {
 	const { isAuthenticated } = useAuthStore();
 	console.log(isAuthenticated);
 
 	return isAuthenticated ? <>{children}</> : <Navigate to='/auth/login' />;
 };
+
+export default PrivateRoute;
