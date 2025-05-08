1b37eda13dd41405cbfb09688bcf240520801a1e
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-05-09 03:46:35 +0500

    add netlify toml file

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			refetchOnWindowFocus: false,
+			refetchOnWindowFocus: true,
 			refetchOnReconnect: true,
 			retry: false,
 			staleTime: 5000,
