2ea32047feef5bfa01cb4891868eb3d32ebddc55
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-06-18 13:32:37 +0500

    fix: update API base URL to use secure HTTPS endpoint

--- src/config/auth/api.jsx
+++ src/config/auth/api.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 // https://api.kelishamiz.uz/api/docs
 // 123123123
 const api = axios.create({
-  baseURL: "http://185.191.141.159",
+  baseURL: "https://api.kelishamiz.uz",
   headers: {
     "Content-Type": "application/json",
   },
