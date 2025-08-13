120ad7d07792013cb628dd3f0a4aa08180dd4da2
baxtiyorqurbonnazarov33 <baxtiyorqurbonnazarov33@gmail.com>
2025-08-13 12:57:20 +0500

    fix

--- src/config/auth/api.jsx
+++ src/config/auth/api.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 // https://api.kelishamiz.uz/api/docs
 // 123123123
 const api = axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL: "https://api.kelishamiz.uz",
   headers: {
     "Content-Type": "application/json",
   },
